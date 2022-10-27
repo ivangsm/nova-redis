c5c884a73f1d019bb06dcdf86fc5e06b5ad65748
Iván Salazar <ivangio.salazar@gmail.com>
2022-10-27 03:03:34 -0500

    🚀

---
+++ src/fcourse_router.erl
@@ -0,0 +1,25 @@
+-module(fcourse_router).
+-behaviour(nova_router).
+
+-export([
+    routes/1
+]).
+
+%% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
+routes(_Environment) ->
+    %% you can add a prefix to your routes
+    [
+        #{
+            prefix => "/users",
+            security => false,
+            routes => [
+                {"/", {fcourse_main_controller, index}, #{methods => [options, get]}},
+                {"/add", {fcourse_main_controller, add}, #{methods => [post]}},
+                {"/update", {fcourse_main_controller, update}, #{methods => [update]}},
+                {"/delete", {fcourse_main_controller, delete}, #{methods => [delete]}},
+                {"/get", {fcourse_main_controller, get}, #{methods => [get]}},
+                {"/getall", {fcourse_main_controller, getall}, #{methods => [get]}},
+                {"/assets/[...]", "assets"}
+            ]
+        }
+    ].
